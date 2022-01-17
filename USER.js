d92cf3e3572d01e4d85c0d2d953632bef3aafb65
Darkwebhacks <pillssdarknet@gmail.com>
2022-01-17 17:16:42 +0530

    ANONYMOUS,JS,CSS

---
+++ USER.js
@@ -0,0 +1,31 @@
+alert("GATE TO ANONYMOUS THING");
+
+var age = window.prompt ("whats your age");
+console.log("Happy Birthday",age);
+document.getElementbyid("myButton").onclick = function () {
+  var Myname = document.getElementbyid("myHacking").value;
+  
+  console.log("I GOT IT",Myname);
+  
+  function sayhello() {
+    console.log(myNOTE);
+    console.log(note1);
+    console.log(note2);
+    
+return
+
+var myNOTE ="DON'T TRY TO TRACK ME  if you try to track me so..";
+var note1 ="this file automatically start malware in your device..";
+var note2 ="and if you do any smart thing so it will shutdown your device";
+var note3 ="in 1.1 second";
+
+sayhello();
+
+  };
+  
+
+}
+
+
+
+
